363e6363d5b080f4ce51ec59b0d8c1121c389592
German Cruz <cruzgerman216@yahoo.com>
2020-05-14 12:53:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :'../views/super_hero'
+    end
+
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:hero].each do |hero|
+        Superhero.new(hero)
+      end
+      @hero = Superhero.all
+      erb :'../views/team'
+    end
 
 end
