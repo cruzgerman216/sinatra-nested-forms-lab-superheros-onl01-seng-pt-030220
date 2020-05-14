a51a80f3f34f41b5936e02ad10edeeff4b42bcd9
German Cruz <cruzgerman216@yahoo.com>
2020-05-14 12:55:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,12 @@ class App < Sinatra::Base
 
 
     post '/teams' do
-      @team_name = params["team"]["name"]
-     @team_motto = params["team"]["motto"]
-     @team_members = params["team"]["members"]
+      @team = Team.new(params[:team])
 
+      params[:team][:hero].each do |hero|
+        Superhero.new(hero)
+      end
+      @hero = Superhero.all
       erb :'../views/team'
     end
 
